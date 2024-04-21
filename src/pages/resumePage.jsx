e4fbae94c31fdec270c13b1f984cd6557b60f1dd
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-04-20 20:43:25 -0400

    added resume page

--- src/pages/resumePage.jsx
+++ src/pages/resumePage.jsx
@@ -1,7 +1,15 @@
+import ResumeElement from "../components/resume";
+
 export default function Resume() {
     return (
-        <main>
-            <p>My Resume</p>
+        <main className="block mx-3 pb-6">
+            <h3 className="subtitle is-2">My Resume</h3>
+            <div className="box m-2 mx-5 has-background-primary-100">
+                <ResumeElement />
+            </div>
+            <div className="is-flex is-justify-content-center">
+                <a href="../assets/IanResume.pdf" download={"IanResume_04-20-2024.pdf"} className="button is-link">Download Resume PDF</a>
+            </div>
         </main>
     );
 }
