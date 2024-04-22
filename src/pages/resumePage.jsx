6b2e208e1d8bed584aba277fab39c624b35608f2
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-04-22 18:35:28 -0400

    small change, added backup download button

--- src/pages/resumePage.jsx
+++ src/pages/resumePage.jsx
@@ -3,9 +3,12 @@ export default function Resume() {
         <main className="block mx-6 pb-2 mb-6">
             <h3 className="subtitle is-2">My Resume</h3>
             <embed className="resume" src="./assets/IanResume04-20-24.pdf" width={"100%"} style={{aspectRatio: 1, zoom: 95}}></embed>
-            {/* <div className="is-flex is-justify-content-center">
+            <div className="is-flex is-justify-content-center">
+                <p>backup download button if the embed fails or doesn't allow downloads:</p>
+            </div>
+            <div className="is-flex is-justify-content-center">
                 <a href="./assets/IanResume.pdf" download={"IanResume_4-20-24"} className="button is-link">Download Resume PDF</a>
-            </div> */}
+            </div>
         </main>
     );
 }
